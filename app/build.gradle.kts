f1b8e02a0da31f9754d8547e990e9d6a64df8401
THOMAS <thomas_gaelle@hotmail.com>
2026-03-07 14:06:41 +0200

    VERSION 11.0 - Build Android App Bundle (AAB) for Google Play Console

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,9 +19,19 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("keystore.jks")
+            storePassword = "picflick123"
+            keyAlias = "picflick"
+            keyPassword = "picflick123"
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
