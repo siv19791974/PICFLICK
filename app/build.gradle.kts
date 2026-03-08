2c0f9823c33fc1dd9120ed0d92b650fbbe5e1e5f
THOMAS <thomas_gaelle@hotmail.com>
2026-03-08 20:23:25 +0200

    VERSION 14.0 - Restore correct Google Sign-In web client ID

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.picflick.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
