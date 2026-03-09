1e8241aa6766d362299483017e686358b23e946e
THOMAS <thomas_gaelle@hotmail.com>
2026-03-09 21:58:03 +0200

    Set version to 1.0.4 (versionCode 8)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.picflick.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.0.3"
+        versionCode = 8
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
