34e4f1157f7a70267715be34b4f30ad2bfa9782b
THOMAS <thomas_gaelle@hotmail.com>
2026-03-09 21:41:49 +0200

    Bump version to 1.0.5 (versionCode 5) for new upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.picflick.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0"
+        versionCode = 5
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
