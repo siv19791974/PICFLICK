a8da33cc6ce8bf481cd99939a98727a113e25ef7
THOMAS <thomas_gaelle@hotmail.com>
2026-03-10 23:56:44 +0200

    Add missing sp import for font size in FriendsScreen

--- app/src/main/java/com/picflick/app/ui/screens/FriendsScreen.kt
+++ app/src/main/java/com/picflick/app/ui/screens/FriendsScreen.kt
@@ -25,6 +25,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
 import com.picflick.app.data.UserProfile
 import com.picflick.app.ui.components.ListItemShimmer
